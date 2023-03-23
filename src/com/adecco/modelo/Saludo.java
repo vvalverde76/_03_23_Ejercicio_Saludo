7940d0865d3ffb4faad4df00b975eac26f6d4cb8
vvalverde76 <vvalverde76@gmail.com>
2023-03-23 14:27:19 +0100

    ejerciciosaludar2

--- src/com/adecco/modelo/Saludo.java
+++ src/com/adecco/modelo/Saludo.java
@@ -1,8 +1,17 @@
 package com.adecco.modelo;
 
 public class Saludo {
+    private String mensajeSaludo;
     public String saludar(String nombreUsuario){
-        return "Bienvenido al mundo " + nombreUsuario;
+       return mensajeSaludo + nombreUsuario;
 
+   }
+
+    public String getMensajeSaludo() {
+        return mensajeSaludo;
+    }
+
+    public void setMensajeSaludo(String mensajeSaludo) {
+        this.mensajeSaludo = mensajeSaludo;
     }
 }
--- src/com/adecco/presentacion/Main.java
+++ src/com/adecco/presentacion/Main.java
@@ -5,6 +5,12 @@ import com.adecco.modelo.Saludo;
 public class Main {
     public static void main(String[] args) {
         Saludo s1 = new Saludo();
+        s1.setMensajeSaludo("Bienvenido al mundo ");
         System.out.println(s1.saludar("Luis Rodriguez"));
+
+        Saludo s2 = new Saludo();
+        s2.setMensajeSaludo("Bienvenido Mr Marshall");
+        System.out.println(s2.getMensajeSaludo());
     }
+
 }
